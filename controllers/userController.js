35dfd65d16d30126623cbd00215bf4d150d16fc2
Drew Daniels <drew@thedaniels.com>
2022-03-20 13:29:25 -0500

    Now taken to forms when clicking on option hyperlinks.

--- controllers/userController.js
+++ controllers/userController.js
@@ -6,7 +6,7 @@ const async = require('async');
 const { body, validationResult } = require('express-validator');
 
 exports.user_create_get = function(req, res, next) {
-    res.send('Will be added later')
+    res.render('register_user_form');
 }
 
 exports.user_create_post = function(req, res, next) {
@@ -14,14 +14,14 @@ exports.user_create_post = function(req, res, next) {
 }
 
 exports.user_login_get = function(req, res, next) {
-    res.send('Will be added later')
+    res.render('login_form');
 }
 
 exports.user_login_post = function(req, res, next) {
     res.send('Will be added later');
 }
 exports.admin_create_get = function(req, res, next) {
-    res.send('Will be added later')
+    res.render('register_admin_form');
 }
 exports.admin_create_post = function(req, res, next) {
     res.send('Will be added later')
